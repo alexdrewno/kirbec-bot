c377b2027350515508f528a86ad7387b7c7a9648
Alex Drewno <adrewno14@gmail.com>
2021-04-02 16:30:56 -0500

    missing files

---
+++ DiscordBot/firebase_config.py
@@ -0,0 +1,30 @@
+import os
+import json
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+FIREBASE_TYPE = os.getenv('FIREBASE_TYPE')
+FIREBASE_PROJECT_ID = os.getenv('FIREBASE_PROJECT_ID')
+FIREBASE_PRIVATE_KEY_ID = os.getenv('FIREBASE_PRIVATE_KEY_ID')
+FIREBASE_PRIVATE_KEY = os.getenv('FIREBASE_PRIVATE_KEY')
+FIREBASE_CLIENT_EMAIL = os.getenv('FIREBASE_CLIENT_EMAIL')
+FIREBASE_CLIENT_ID = os.getenv('FIREBASE_CLIENT_ID')
+FIREBASE_AUTH_URI = os.getenv('FIREBASE_AUTH_URI')
+FIREBASE_TOKEN_URI = os.getenv('FIREBASE_TOKEN_URI')
+FIREBASE_AUTH_PROVIDER_X509_CERT_URL = os.getenv('FIREBASE_AUTH_PROVIDER_X509_CERT_URL')
+FIREBASE_CLIENT_X509_CERT_URL = os.getenv('FIREBASE_CLIENT_X509_CERT_URL')
+
+firebase_config_dict = {
+  "type": FIREBASE_TYPE,
+  "project_id": FIREBASE_PROJECT_ID,
+  "private_key_id": FIREBASE_PRIVATE_KEY_ID,
+  "private_key": FIREBASE_PRIVATE_KEY.replace('\\n', '\n'),
+  "client_email": FIREBASE_CLIENT_EMAIL,
+  "client_id": FIREBASE_CLIENT_ID,
+  "auth_uri": FIREBASE_AUTH_URI,
+  "token_uri": FIREBASE_TOKEN_URI,
+  "auth_provider_x509_cert_url": FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": FIREBASE_CLIENT_X509_CERT_URL
+}
---
+++ DiscordBot/main.py
@@ -0,0 +1,15 @@
+import discord
+from DiscordClient import DiscordClient
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Main script to start the DiscordClient
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.members = True
+client = DiscordClient(intents=intents)
+client.run(DISCORD_TOKEN)
