449a85e187b7d9dd38da76bbacb0df755ee6e220
Alex Drewno <adrewno14@gmail.com>
2021-04-02 16:32:20 -0500

    Update main.py

--- DiscordBot/main.py
+++ DiscordBot/main.py
@@ -2,9 +2,9 @@ import discord
 from DiscordClient import DiscordClient
 import os
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 # Main script to start the DiscordClient
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
